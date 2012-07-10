babc8467aba1e3f73c1d3e05df617d9d96b08752
Thomas Betrancourt <thomas@betrancourt.net>
2012-07-10 15:48:43 +0200

    Define plugin informations

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :lemonldap do
-  name 'Lemonldap plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'LemonLDAP::NG plugin'
+  author 'Thomas Bétrancourt'
+  description 'Allow users to log in to your site using LemonLDAP::NG authentication'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://projects.rclsilver.net/projects/redmine-lemonldap'
+  author_url 'http://www.betrancourt.net/thomas'
 end
