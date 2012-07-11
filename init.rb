3fa25ac731de6592d00986587424ee50982bbea1
Thomas Betrancourt <thomas@betrancourt.net>
2012-07-11 17:05:11 +0200

    Making the plugin configurable

--- init.rb
+++ init.rb
@@ -1,8 +1,17 @@
-Redmine::Plugin.register :lemonldap do
+Redmine::Plugin.register :redmine_lemonldap do
   name 'LemonLDAP::NG plugin'
   author 'Thomas Bétrancourt'
   description 'Allow users to log in to your site using LemonLDAP::NG authentication'
   version '0.0.1'
   url 'http://projects.rclsilver.net/projects/redmine-lemonldap'
   author_url 'http://www.betrancourt.net/thomas'
+  settings :partial => 'settings/redmine_lemonldap_settings',
+           :default => {
+             'enable' => 'false',
+             'username_env_var' => 'HTTP_AUTH_USER',
+             'firstname_env_var' => 'HTTP_FIRSTNAME',
+             'lastname_env_var' => 'HTTP_LASTNAME',
+             'email_env_var' => 'HTTP_EMAIL',
+             'auto_registration' => 'false'
+           }
 end
